b62eacdd170689516a57adb952cb919b44023200
Akash Kalamkar <akash.kalamkar1995@gmail.com>
2026-05-15 10:51:05 +0530

    line 12 myself

--- src/main/java/com/demo/javadevopsproject/Login.java
+++ src/main/java/com/demo/javadevopsproject/Login.java
@@ -9,6 +9,7 @@ public class Login {
         System.out.println("adding line 9 by myself");
         System.out.println("line 9 added by another dev");
        System.out.println("line 11 added by another dev");
+       System.out.println("line 12 by myself");
        System.out.println("line 12 by anotehr dev");
     }
 }
